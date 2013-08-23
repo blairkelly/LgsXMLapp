ae54057854c04ee12c9514a6b78af6d2288aed6d
Blair Kelly <bkelly@klick.com>
2013-08-23 17:35:22 -0400

    First commit

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ LgsXMLapp
 =========
 
 For editing XML
+
+
+npm install socket.io
+- if necessary
+
+make sure you have appropriate permissions set on working folder
+
+
+
+Links:
+http://nodejs.org/docs/latest/api/fs.html
+http://stackoverflow.com/questions/2496710/nodejs-write-to-file
+http://stackoverflow.com/questions/9914816/what-is-an-example-of-the-simplest-possible-socket-io-example
---
+++ app.js
@@ -0,0 +1,42 @@
+var http = require('http'),
+    fs = require('fs'),
+    // NEVER use a Sync function except at start-up!
+    index = fs.readFileSync(__dirname + '/index.html');
+
+// Send index.html to all requests
+var app = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(index);
+});
+
+function wf(thetime) {
+    var fs = require('fs');
+    fs.writeFile("the.xml", "The date is: " + thetime, function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("Saved Agin.");
+        }
+    });
+}
+
+// Socket.io server listens to our app
+var io = require('socket.io').listen(app);
+
+// Send current time to all connected clients
+function sendTime() {
+    var thetime = new Date().toJSON();
+    io.sockets.emit('time', { time: thetime });
+    wf(thetime);
+}
+
+// Send current time every 10 secs
+setInterval(sendTime, 10000);
+
+// Emit welcome message on connection
+io.sockets.on('connection', function(socket) {
+    socket.emit('welcome', { message: 'Hi there.', time: new Date().toJSON() });
+    socket.on('i am client', console.log);
+});
+
+app.listen(3000);
